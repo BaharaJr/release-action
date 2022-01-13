8519b015769e74293c866ce6c1cc1ac0a488a233
Bennett <bennybenester@gmail.com>
2022-01-13 15:13:25 +0300

    change octokit link

--- src/action.js
+++ src/action.js
@@ -4,11 +4,12 @@ const io = require("@actions/io");
 
 async function run() {
   const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
-  console.log("TOKEN::", GITHUB_TOKEN);
   const octokit = github.getOctokit(GITHUB_TOKEN);
-  const { context } = github;
+  console.log("OCTO", octokit);
+  const { context = {} } = github;
   const { pull_request } = context.payload;
-  await octokit.issues.createComment({
+
+  await octokit.rest.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
     body: "Hey Bennett, we are up on a good path",
