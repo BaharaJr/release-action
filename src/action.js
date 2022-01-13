8253f40a2ab3d279e711ef4a9859da5dc578126d
Bennett <bennybenester@gmail.com>
2022-01-13 16:36:25 +0300

    get commits from PR

--- src/action.js
+++ src/action.js
@@ -1,13 +1,16 @@
 const github = require("@actions/github");
 const core = require("@actions/core");
 const io = require("@actions/io");
+const axios = require("axios");
 
 async function run() {
   const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
   const octokit = github.getOctokit(GITHUB_TOKEN);
   const { context = {} } = github;
   const { pull_request } = context.payload;
-  console.log(JSON.stringify(pull_request));
+  console.log(pull_request);
+  // const commits = (await axios.get(pull_request.commits_url)).data;
+
   await octokit.rest.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
