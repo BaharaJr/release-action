a17314930953e5449348b5225becad0b14b41c84
Bennett <bennybenester@gmail.com>
2022-01-13 14:48:12 +0300

    add github core api access

--- src/action.js
+++ src/action.js
@@ -1,4 +1,16 @@
+const github = require("@actions/github");
+const core = require("@actions/core");
+const io = require("@actions/io");
+
 async function run() {
-  console.log("Yoooh");
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+  const { context } = github;
+  const { pull_request } = context.payload;
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: "Hey Bennett, we are up on a good path",
+  });
 }
-return run();
+run();
