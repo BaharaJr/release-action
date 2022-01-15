b2b0678c8dd51d81085e9299083812c912366d51
Bennett <bennybenester@gmail.com>
2022-01-15 18:56:22 +0300

    get latest commits from PR

--- src/action.js
+++ src/action.js
@@ -1,42 +1,46 @@
 /* eslint-disable camelcase */
 const github = require('@actions/github');
 const core = require('@actions/core');
-const axios = require('axios');
 
 async function run() {
   const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
   const octokit = github.getOctokit(GITHUB_TOKEN);
   const { context = {} } = github;
   const { pull_request } = context.payload;
-  const commits = (
-    await axios.get(pull_request.commits_url, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-        'User-Agent': pull_request.head.repo.name,
-      },
-    })
-  ).data;
+  const commits = await octokit.request(
+    `GET /repos/${pull_request.head.repo.owner.login}/${pull_request.head.repo.name}/commits`,
+    {
+      owner: pull_request.head.repo.owner.login,
+      repo: pull_request.head.repo.name,
+    },
+  );
+
+  const lastestRelease = await octokit.request(
+    `GET /repos/${pull_request.head.repo.owner.login}/${pull_request.head.repo.name}/releases/latest`,
+    {
+      owner: pull_request.head.repo.owner.login,
+      repo: pull_request.head.repo.name,
+    },
+  );
 
   const releases = await octokit.request(
     `POST /repos/${pull_request.head.repo.owner.login}/${pull_request.head.repo.name}/releases`,
     {
       owner: pull_request.head.repo.owner.login,
       repo: pull_request.head.repo.name,
-      tag_name: 'TEST-RELEASE1',
-      body: `## What's Changed\n* ${(commits || [])
-        .map((value) => `${value.commit.message} in ${value.html_url}`)
-        .join('\n*')}`,
-      generate_release_notes: true,
+      tag_name: 'TEST-1',
+      body: `## What's Changed from last release\n * ${(commits || [])
+        .map((value) => `${value.commit.message} ${value.html_url}`)
+        .join('\n *')}`,
     },
   );
 
-  console.log(JSON.stringify(releases));
 
   await octokit.rest.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
     body: `${(commits || [])
-      .map((value) => `${value.commit.message} in ${value.html_url}`)
+      .map((value) => `${value.commit.message} ${value.html_url}`)
       .join('\n')}`,
   });
 }
