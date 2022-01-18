bac7f69eb91e06676a4494ea2ebbfe05f7ad67a5
Bennett <bennybenester@gmail.com>
2022-01-18 17:43:41 +0300

    send assets to github

--- src/action.js
+++ src/action.js
@@ -2,9 +2,13 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
 const axios = require('axios');
+const fs = require('fs');
 
 async function run() {
   const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  const FILE_LOCATION = core.getInput('FILE_LOCATION');
+  const ASSET_NAME = core.getInput('ASSET_NAME');
+  const ASSET_TYPE = core.getInput('ASSET_TYPE');
   const octokit = github.getOctokit(GITHUB_TOKEN);
   const { context = {} } = github;
   const { pull_request } = context.payload;
@@ -18,12 +22,22 @@ async function run() {
     })
   ).data;
 
+  const contentLength = (filePath) => fs.statSync(filePath).size;
+
+  await octokit.rest.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: `${(commits || [])
+      .map((value) => `${value.commit.message} ${value.html_url}`)
+      .join('\n* ')}`,
+  });
+
   const lastestRelease = await octokit.request(
     `GET /repos/${pull_request.head.repo.owner.login}/${pull_request.head.repo.name}/releases/latest`,
     {
       owner: pull_request.head.repo.owner.login,
       repo: pull_request.head.repo.name,
-    }
+    },
   );
 
   const releases = await octokit.request(
@@ -36,15 +50,19 @@ async function run() {
         .map((value) => `${value.commit.message} ${value.html_url}`)
         .join('\n* ')}`,
       generate_release_notes: true,
-    }
+    },
   );
+  const headers = {
+    'content-type': ASSET_TYPE,
+    'content-length': contentLength(FILE_LOCATION),
+  };
 
-  await octokit.rest.issues.createComment({
-    ...context.repo,
-    issue_number: pull_request.number,
-    body: `${(commits || [])
-      .map((value) => `${value.commit.message} ${value.html_url}`)
-      .join('\n* ')}`,
+  const uploadAssetResponse = await github.repos.uploadReleaseAsset({
+    url: releases.upload_url,
+    headers,
+    name: ASSET_NAME || 'CUSTOM_ASSET',
+    file: fs.readFileSync(FILE_LOCATION),
   });
+  console.log(JSON.stringify(uploadAssetResponse));
 }
 run();
